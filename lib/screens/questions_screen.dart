ed2dfc51eea292657d50c011f901e0ceba7a4145
Hamid Ali <hamidalig942@gmail.com>
2026-04-06 17:49:33 +0500

    add: styling in questions screen

--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:flutter_quiz/data/questions.dart';
 import 'package:flutter_quiz/widgets/answer_button.dart';
@@ -12,15 +13,27 @@ class QuestionsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(questions[0].text),
-          ...questions[0].answers.map(
-            (answer) => AnswerButton(text: answer, onPressed: answerQuestion),
-          ),
-        ],
+    return Container(
+      margin: EdgeInsets.all(40),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              questions[0].text,
+              style: GoogleFonts.lato(
+                color: Color.fromARGB(255, 201, 153, 251),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30),
+            ...questions[0].answers.map(
+              (answer) => AnswerButton(text: answer, onPressed: answerQuestion),
+            ),
+          ],
+        ),
       ),
     );
   }
