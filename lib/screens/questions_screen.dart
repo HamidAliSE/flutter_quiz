fd9378ef7146d76052d666da14e3d56f159bc9de
Hamid Ali <hamidalig942@gmail.com>
2026-04-06 17:32:52 +0500

    add: first question UI

--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_quiz/data/questions.dart';
+
 class QuestionsScreen extends StatelessWidget {
   const QuestionsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Questions Screen'));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(questions[0].text),
+          ...questions[0].answers.map((answer) => Text(answer)),
+        ],
+      ),
+    );
   }
 }
