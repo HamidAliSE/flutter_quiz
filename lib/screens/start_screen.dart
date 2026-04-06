6c973c8d164d04ff8136ff618ba4294629616f61
Hamid Ali <hamidalig942@gmail.com>
2026-04-06 17:13:54 +0500

    add: styling to start screen

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -9,12 +9,26 @@ class StartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png'),
-          Text('Learn Flutter the fun way!'),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          SizedBox(height: 80),
+          Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 237, 223, 252),
+              fontSize: 24,
+            ),
+          ),
+          SizedBox(height: 30),
           OutlinedButton.icon(
             onPressed: startQuiz,
-            icon: Icon(Icons.arrow_forward),
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: Icon(Icons.arrow_right_alt),
             label: Text('Start Quiz'),
           ),
         ],
